90dcaa714181c2e8e9fb7dd9fafd0a3c8fc4e068
Piotr Olesinski <121697333+PiotrOlesinski@users.noreply.github.com>
2023-01-15 18:31:53 +0000

    Still initial

--- PJO-SAURUS/docusaurus.config.js
+++ PJO-SAURUS/docusaurus.config.js
@@ -8,6 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
+
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
